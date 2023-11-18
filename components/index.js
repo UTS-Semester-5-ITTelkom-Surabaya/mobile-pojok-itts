956877422828090744430231ae824e6ba9420b6c
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-18 17:29:06 +0700

    refactor: export all components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import ActionButton from './ActionButton';
+import BackButton from './BackButton';
+import ArticleTile from './ArticleTile';
+import SocialMediaTile from './SocialMediaTile';
+import SizedBox from './SizedBox';
+
+export { ActionButton, BackButton, ArticleTile, SocialMediaTile, SizedBox };
