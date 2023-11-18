3c28785fb182afb052a0bf3a69038a285b6a9c48
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-18 17:34:37 +0700

    refactor: export all components

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import ArticleDetailScreen from './ArticleDetailScreen';
+import ArticlesScreen from './ArticlesScreen';
+import WelcomeScreen from './WelcomeScreen';
+
+export { ArticleDetailScreen, ArticlesScreen, WelcomeScreen };
